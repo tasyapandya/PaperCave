1d6573b83e5a55c1ecc089b01d9a10867d096028
Tasya Pandya <tasya.pandya@gmail.com>
2026-09-03 13:23:52 +0700

    Initial Commit

---
+++ PaperCave/PaperCaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperCaveApp.swift
+//  PaperCave
+//
+//  Created by Tasya Pandya Latifa on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PaperCaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
